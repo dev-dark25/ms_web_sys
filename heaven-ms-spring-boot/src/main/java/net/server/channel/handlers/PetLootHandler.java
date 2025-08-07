11880b68ca89429220a7bc1f23b80f5134631bc5
dev-dark25 <283648851@qq.com>
2025-08-07 16:57:27 +0800

    Add files via upload

---
+++ heaven-ms-spring-boot/src/main/java/net/server/channel/handlers/PetLootHandler.java
@@ -0,0 +1,107 @@
+/*
+	This file is part of the OdinMS Maple Story Server
+    Copyright (C) 2008 Patrick Huy <patrick.huy@frz.cc>
+		       Matthias Butz <matze@odinms.de>
+		       Jan Christian Meyer <vimes@odinms.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation version 3 as published by
+    the Free Software Foundation. You may not use, modify or distribute
+    this program under any other version of the GNU Affero General Public
+    License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package net.server.channel.handlers;
+
+import java.util.List;
+import java.util.Set;
+
+import client.MapleCharacter;
+import client.MapleClient;
+import client.inventory.MaplePet;
+
+import net.AbstractMaplePacketHandler;
+
+import server.maps.MapleMap;
+import server.maps.MapleMapItem;
+import server.maps.MapleMapObject;
+import server.maps.MapleMapObjectType;
+import tools.MaplePacketCreator;
+import tools.data.input.SeekableLittleEndianAccessor;
+
+/**
+ * @author TheRamon
+ * @author Ronan
+ */
+public final class PetLootHandler extends AbstractMaplePacketHandler {
+    @Override
+    public final void handlePacket(SeekableLittleEndianAccessor slea, MapleClient c) {
+        MapleCharacter chr = c.getPlayer();
+        
+        int petIndex = chr.getPetIndex(slea.readInt());
+        MaplePet pet = chr.getPet(petIndex);
+        if (pet == null || !pet.isSummoned()) {
+            c.announce(MaplePacketCreator.enableActions());
+            return;
+        }
+        
+        slea.skip(13);
+        int oid = slea.readInt();
+        MapleMapObject ob = chr.getMap().getMapObject(oid);        
+        try {
+            MapleMapItem mapitem = (MapleMapItem) ob;
+            if (mapitem.getMeso() > 0) {
+                if (!chr.isEquippedMesoMagnet()) {
+                    c.announce(MaplePacketCreator.enableActions());
+                    return;
+                }
+
+                if (chr.isEquippedPetItemIgnore()) {
+                    final Set<Integer> petIgnore = chr.getExcludedItems();
+                    if(!petIgnore.isEmpty() && petIgnore.contains(Integer.MAX_VALUE)) {
+                        c.announce(MaplePacketCreator.enableActions());
+                        return;
+                    }
+                }
+            } else {
+                if (!chr.isEquippedItemPouch()) {
+                    c.announce(MaplePacketCreator.enableActions());
+                    return;
+                }
+
+                if (chr.isEquippedPetItemIgnore()) {
+                    final Set<Integer> petIgnore = chr.getExcludedItems();
+                    if(!petIgnore.isEmpty() && petIgnore.contains(mapitem.getItem().getItemId())) {
+                        c.announce(MaplePacketCreator.enableActions());
+                        return;
+                    }
+                }
+            }
+            boolean petFullPick = chr.getPetFullPick();
+            if (petFullPick) {
+                MapleMap map = chr.getMap();
+                List<MapleMapObject> items = map.getMapObjects();
+                for (int i = 0; i < items.size(); i++) {
+                    MapleMapObject obj = items.get(i);
+                    if (MapleMapObjectType.ITEM.equals(obj.getType())) {
+                        // 玩家自己丢出来的东西不吸
+                        chr.pickupItemWithoutSelfDrop(map.getMapObject(obj.getObjectId()), petIndex);
+                    }
+                }
+            } else {
+                chr.pickupItem(ob, petIndex);
+            }
+
+        } catch (NullPointerException | ClassCastException e) {
+            c.announce(MaplePacketCreator.enableActions());
+        }
+    }
+}
