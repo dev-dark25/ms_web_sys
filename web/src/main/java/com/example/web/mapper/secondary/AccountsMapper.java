da99555d4149bc863f0d8af7a61d7fe87534b1e7
dev-dark25 <283648851@qq.com>
2025-08-24 18:14:58 +0800

    Add files via upload

--- web/src/main/java/com/example/web/mapper/secondary/AccountsMapper.java
+++ web/src/main/java/com/example/web/mapper/secondary/AccountsMapper.java
@@ -4,14 +4,17 @@ import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
 
 @Component
 @Mapper
 public interface AccountsMapper {
 
-    List selectAll();
+    List selectAll(Map req);
 
     int selectCountByLoggedin();
 
     List selectById(int id);
+
+    void update(Map req);
 }
