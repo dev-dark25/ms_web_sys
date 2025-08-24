ceff94d90278f6cc82a9875f397b4b56a6f3c0aa
dev-dark25 <283648851@qq.com>
2025-08-24 18:05:12 +0800

    Add files via upload

--- vue/src/views/MapleStory/Accounts.vue
+++ vue/src/views/MapleStory/Accounts.vue
@@ -1,50 +1,50 @@
 <template>
   <div class="default_div">
-      <h2>Account</h2>
-      <el-table v-loading="loading" element-loading-text="Loading..." :data="filterTableData" height="300"
-        style="width: 100%">
-        <el-table-column fixed prop="id" label="id" />
-        <el-table-column prop="name" label="name" />
-        <el-table-column prop="password" label="password" width="460" />
-        <el-table-column prop="pin" label="pin" />
-        <el-table-column prop="pic" label="pic" />
-        <el-table-column prop="loggedin" label="loggedin" width="100" />
-        <el-table-column prop="lastlogin" label="lastlogin" width="160" />
-        <el-table-column prop="createdat" label="createdat" width="160" />
-        <el-table-column prop="birthday" label="birthday" width="100" />
-        <el-table-column prop="banned" label="banned" />
-        <el-table-column prop="banreason" label="banreason" width="100" />
-        <el-table-column prop="macs" label="macs" width="280" />
-        <el-table-column prop="nxCredit" label="nxCredit" width="100" />
-        <el-table-column prop="maplePoint" label="maplePoint" width="100" />
-        <el-table-column prop="nxPrepaid" label="nxPrepaid" width="100" />
-        <el-table-column prop="characterslots" label="characterslots" width="120" />
-        <el-table-column prop="gender" label="gender" />
-        <el-table-column prop="tempban" label="tempban" width="160" />
-        <el-table-column prop="greason" label="greason" />
-        <el-table-column prop="tos" label="tos" />
-        <el-table-column prop="sitelogged" label="sitelogged" width="100" />
-        <el-table-column prop="webadmin" label="webadmin" width="100" />
-        <el-table-column prop="nick" label="nick" />
-        <el-table-column prop="mute" label="mute" />
-        <el-table-column prop="email" label="email" width="120" />
-        <el-table-column prop="ip" label="ip" />
-        <el-table-column prop="rewardpoints" label="rewardpoints" width="120" />
-        <el-table-column prop="votepoints" label="votepoints" width="100" />
-        <el-table-column prop="hwid" label="hwid" width="100" />
-        <el-table-column prop="language" label="language" width="100" />
-        <el-table-column fixed="right" label="Operations" width="180">
-          <template #header>
-            <el-input v-model="search" size="small" placeholder="Type to search name" />
-          </template>
-          <template #default="scope">
-            <el-button size="small" @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
-            <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
+    <h2>Account</h2>
+    <el-table v-loading="loading" element-loading-text="Loading..." :data="filterTableData" height="300"
+      style="width: 100%">
+      <el-table-column fixed prop="id" label="id" />
+      <el-table-column prop="name" label="name" />
+      <el-table-column prop="password" label="password" width="460" />
+      <el-table-column prop="pin" label="pin" />
+      <el-table-column prop="pic" label="pic" />
+      <el-table-column prop="loggedin" label="loggedin" width="100" />
+      <el-table-column prop="lastlogin" label="lastlogin" width="160" />
+      <el-table-column prop="createdat" label="createdat" width="160" />
+      <el-table-column prop="birthday" label="birthday" width="100" />
+      <el-table-column prop="banned" label="banned" />
+      <el-table-column prop="banreason" label="banreason" width="100" />
+      <el-table-column prop="macs" label="macs" width="280" />
+      <el-table-column prop="nxCredit" label="nxCredit" width="100" />
+      <el-table-column prop="maplePoint" label="maplePoint" width="100" />
+      <el-table-column prop="nxPrepaid" label="nxPrepaid" width="100" />
+      <el-table-column prop="characterslots" label="characterslots" width="120" />
+      <el-table-column prop="gender" label="gender" />
+      <el-table-column prop="tempban" label="tempban" width="160" />
+      <el-table-column prop="greason" label="greason" />
+      <el-table-column prop="tos" label="tos" />
+      <el-table-column prop="sitelogged" label="sitelogged" width="100" />
+      <el-table-column prop="webadmin" label="webadmin" width="100" />
+      <el-table-column prop="nick" label="nick" />
+      <el-table-column prop="mute" label="mute" />
+      <el-table-column prop="email" label="email" width="120" />
+      <el-table-column prop="ip" label="ip" />
+      <el-table-column prop="rewardpoints" label="rewardpoints" width="120" />
+      <el-table-column prop="votepoints" label="votepoints" width="100" />
+      <el-table-column prop="hwid" label="hwid" width="100" />
+      <el-table-column prop="language" label="language" width="100" />
+      <el-table-column fixed="right" label="Operations" width="180">
+        <template #header>
+          <el-input v-model="search" size="small" placeholder="Type to search name" />
+        </template>
+        <template #default="scope">
+          <el-button size="small" @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+          <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
 
-      <!-- <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize" :page-sizes="[10, 20, 30, 40]"
+    <!-- <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize" :page-sizes="[10, 20, 30, 40]"
       :small="small" :disabled="disabled" :background="background" layout="total, sizes, prev, pager, next, jumper"
       :total="total" @size-change="handleSizeChange" @current-change="handleCurrentChange" />
     <div class="flex items-center mb-4">
@@ -103,8 +103,9 @@ const handleDelete = (index: number, row: any) => {
 
 
 const getAccounts = async () => {
+  const req = {}
   try {
-    const res = await getMSAccounts();
+    const res = await getMSAccounts(req);
     if (res.code === '200') {
       list.splice(0, list.length, ...res.list);
       loading.value = false;
