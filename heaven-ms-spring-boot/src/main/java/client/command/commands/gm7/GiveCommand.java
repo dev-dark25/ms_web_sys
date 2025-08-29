b2b9e3fa287f8ceea8f5d353d23e860145fe6b43
dev-dark25 <283648851@qq.com>
2025-08-29 16:53:09 +0800

    Add files via upload

--- heaven-ms-spring-boot/src/main/java/client/command/commands/gm7/GiveCommand.java
+++ heaven-ms-spring-boot/src/main/java/client/command/commands/gm7/GiveCommand.java
@@ -3,6 +3,8 @@ package client.command.commands.gm7;
 import client.MapleCharacter;
 import client.MapleClient;
 import client.command.Command;
+import client.inventory.Equip;
+import server.MapleItemInformationProvider;
 
 public class GiveCommand extends Command {
     {
@@ -14,16 +16,24 @@ public class GiveCommand extends Command {
         MapleCharacter player = c.getPlayer();
 
         if (params.length < 3) {
-            player.yellowMessage("输入: @give <type: meso/nv/exp> <characterId> <count>");
+            player.yellowMessage("输入: @give <type: meso/nx/exp/item/equip> <characterId> <count> <itemId/equipId> <str|dex|int|luk|hp|mp|watk|matk|wdef|mdef|acc|avoid|speed|jump>");
             return;
         }
 
         String type;
-        int characterId, count;
+        int characterId, count, item = 0;
+        String[] equip = new String[14];
         try {
             type = params[0];
             characterId = Integer.parseInt(params[1]);
             count = Integer.parseInt(params[2]);
+            if (params.length == 4) {
+                item = Integer.parseInt(params[3]);
+            }
+            if (params.length == 5) {
+                item = Integer.parseInt(params[3]);
+                equip = params[4].split("\\|");
+            }
         } catch (NumberFormatException e) {
             player.dropMessage("请输入正确的参数");
             return;
@@ -49,6 +59,26 @@ public class GiveCommand extends Command {
             targetCharacter.gainExp(count);
             targetCharacter.startMapEffect("管理员给你发送了" + count + "经验！", 5120015);
             player.message(targetCharacter.getName() + " 已收到经验：" + count);
+        } else if ("item".equals(type)) {
+            targetCharacter.getAbstractPlayerInteraction().gainItem(item, (short) (count));
+            String itemName = MapleItemInformationProvider.getInstance().getName(item);
+            targetCharacter.startMapEffect("管理员给你发送了道具：" + itemName + count + "个", 5120015);
+            player.message(targetCharacter.getName() + " 已收到道具：" + itemName + count + "个");
+        } else if ("equip".equals(type)) {
+            if (params.length == 5) {
+                Equip eqp = (Equip) targetCharacter.getAbstractPlayerInteraction().gainEquip(item, Short.parseShort(equip[0]), Short.parseShort(equip[1]),
+                        Short.parseShort(equip[2]), Short.parseShort(equip[3]), Short.parseShort(equip[4]), Short.parseShort(equip[5]),
+                        Short.parseShort(equip[6]), Short.parseShort(equip[7]), Short.parseShort(equip[8]), Short.parseShort(equip[9]),
+                        Short.parseShort(equip[10]), Short.parseShort(equip[11]), Short.parseShort(equip[12]), Short.parseShort(equip[13]), -1L, (short) 1);
+                targetCharacter.startMapEffect("管理员给你发送了装备：" + eqp, 5120015);
+                player.message(targetCharacter.getName() + " 已收到装备：" + eqp);
+            } else {
+                Equip eqp = (Equip) targetCharacter.getAbstractPlayerInteraction().gainEquip(item, null, null,
+                        null, null, null, null, null, null, null, null,
+                        null, null, null, null, -1L, (short) 1);
+                targetCharacter.startMapEffect("管理员给你发送了装备：" + eqp, 5120015);
+                player.message(targetCharacter.getName() + " 已收到装备：" + eqp);
+            }
         } else {
 
         }
